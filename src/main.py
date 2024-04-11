208f198349859aa01f4bd41655a944b6373da1e1
julialfk <j.liemfk@gmail.com>
2024-04-11 22:29:25 +0200

    Added basic codebert test

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
-def main():
+from transformers import pipeline
 
+def main():
+    classifier = pipeline("sentiment-analysis", model="microsoft/codebert-base")
+    print(classifier("We are very happy to show you the 🤗 Transformers library."))
 
 if __name__ == "__main__":
     main()
